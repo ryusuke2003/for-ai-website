cc85f7cd5134ac3643bef3c725766f9e83d1fcae
ryusuke2003 <r-kawata@digeon.co>
2026-09-14 01:41:08 +0900

    Tray表示ごとのナビゲーション通知を追加

--- src/desktop/trayNavigation.js
+++ src/desktop/trayNavigation.js
@@ -1,6 +1,8 @@
 import { isTauri } from '@tauri-apps/api/core';
 import { listen } from '@tauri-apps/api/event';
 
+export const TRAY_NAVIGATION_APPLIED_EVENT = 'one:tray-navigation-applied';
+
 const TARGET_HASH = Object.freeze({
   timer: '',
   todo: '#todo',
@@ -11,6 +13,7 @@ const TARGET_HASH = Object.freeze({
 export function applyTrayNavigation(target) {
   if (!(target in TARGET_HASH)) return false;
   window.location.hash = TARGET_HASH[target];
+  window.dispatchEvent(new CustomEvent(TRAY_NAVIGATION_APPLIED_EVENT, { detail: target }));
   return true;
 }
 
