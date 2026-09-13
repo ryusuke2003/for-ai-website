fbf6f934500f121fbb9b41811f88f555ebe8672d
ryusuke2003 <r-kawata@digeon.co>
2026-09-13 22:53:00 +0900

    Trayタイマー操作のlistenerを起動する

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './App.jsx';
+import { startTrayTimerActions } from './desktop/trayTimerActions.js';
 import { startTrayTimerSync } from './desktop/trayTimerSync.js';
 import './tailwind.css';
 
@@ -8,6 +9,7 @@ delete globalThis.ONE_TIMER_RUNTIME;
 delete globalThis.ONE_PROGRESS_RUNTIME;
 
 startTrayTimerSync();
+void startTrayTimerActions();
 
 function mountApp() {
   const root = document.querySelector('#root');
