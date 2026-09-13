0164986f0e1619b6d2d1c1cb31c46b09b7cf7a68
ryusuke2003 <r-kawata@digeon.co>
2026-09-13 14:09:59 +0900

    旧タスク互換ヘルパーを削除

--- backup-timer-context-compat.js
+++
@@ -1,6 +0,0 @@
-function hasActiveDailyTaskContext() {
-  const fullDuration = selectedMinutes * 60;
-  return timerId !== null
-    || completionReady
-    || (remainingSeconds > 0 && remainingSeconds < fullDuration);
-}
